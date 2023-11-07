121847cd17cf29284887f0d33e6911319a600c77
sean <admin@koji-projects.com>
2023-11-07 12:00:09 -0600

    Update index.js

--- templates/basic-google-search/index.js
+++ templates/basic-google-search/index.js
@@ -1,18 +1,25 @@
-// Get the configuration
-let {
-  query,
-  outputVariable,
-} = ai.config;
+/**
+ * Get the query.
+ *
+ * Note: While we could use ai.config.query to get the query,
+ * ai.getConfig(configVariableName) will automatically resolve
+ * the value if the provided value references another variable.
+ *
+ * For example, if you just wanted to search for "cats" every
+ * time this function runs, you could set query to "cats" in
+ * the function configuration and get it with ai.config.query.
+ *
+ * But if you to use a User Input block to get a query from
+ * a user and save it as variable myQuery, you would type
+ * {{myQuery}} in the function configuration. But then
+ * ai.config.query would return the literal value "{{myQuery}}".
+ * ai.getConfig('query'), on the other hand, will automatically
+ * resolve {{myQuery}} to whatever the user's query is.
+ */
+const query = ai.getConfig('query');
 
-// If the query is a variable, resolve it
-if (query.startsWith('{{')) {
-  const queryVariableName = query
-    .replace('{{', '')
-    .replace('}}', '');
-  query = ai.vars[queryVariableName];
-}
-
-// Make sure there is a query
+// Make sure there is a query. Otherwise, we have nothing to
+// search for.
 if (!query) {
   console.log('No query defined');
   return;
@@ -23,11 +30,15 @@ const result = await ai.searchGoogle(query);
 
 /**
  * Searching returns an object with two keys:
- *  - text: The search result page structured as text, which makes it easy to use as a variable in a message
- * - results: The results structured as an array of { title, description, url }, for more advanced processing.
+ *  - text:   The search result page structured as text, which
+ *            makes it easy to use as a variable in a message
+ * - results: The results structured as an array of
+ *            { title, description, url }, for more advanced
+ *            processing.
  */
 console.log(result.text);
 console.log(result.results);
 
 // Assign the text result to the variable
+const { outputVariable } = ai.config;
 ai.vars[outputVariable] = result.text;
