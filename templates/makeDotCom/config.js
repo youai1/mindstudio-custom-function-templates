14ff0d4b448fc0e79f8a7fdb24243b8d4dae55be
markof94 <markofajler@gmail.com>
2023-11-21 20:34:24 +0100

    add make.com block

---
+++ templates/makeDotCom/config.js
@@ -0,0 +1,29 @@
+config = {
+  name: "Make Webhook",
+  description: "Send data to a Make webhook.",
+  author: "Marko",
+  blockStyle: {
+    backgroundImageUrl: 'https://youai.imgix.net/images/a8284d01-9ec8-4683-a15d-7f4d7d922e5f_1700594752696.jpg',
+    foregroundColor: '#000000',
+    label: '.',
+  },
+  configurationSections: [
+    {
+      title: 'Configuration',
+      items: [
+        {
+          label: 'Make Webhook URL',
+          variable: 'make_webhook_url',
+          helpText: 'Webhook URL provided by Make.',
+          type: 'text',
+        },
+        {
+          label: 'Input',
+          variable: 'input',
+          type: 'text',
+          helpText: 'Input can be a string or a {{variable}}.',
+        },
+      ],
+    },
+  ],
+}
