666b311821a4e015b0d924b0a5ed856f01ff9f92
Braingearceo <104493450+Braingearceo@users.noreply.github.com>
2023-11-21 12:03:49 +0100

    Create config.js

---
+++ templates/get_stock_news/config.js
@@ -0,0 +1,21 @@
+config = {
+  configurationSections: [
+    {
+      title: 'Custom Block',
+      items: [
+        {
+          label: 'Output Variable',
+          type: 'text',
+          variable: 'outputVar',
+          helpText: 'Save the generated image URL to this variable',
+        },
+        {
+          label: 'Label',
+          type: 'text',
+          variable: 'performanceID',
+          helpText: 'the stock ID',
+        },
+      ],
+    },
+  ],
+}
