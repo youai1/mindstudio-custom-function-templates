8a0d81cae3cc1e92e39e505ecb068d67c9a521d5
markof94 <markofajler@gmail.com>
2023-11-21 22:06:26 +0100

    better descriptions

--- templates/makeDotCom/config.js
+++ templates/makeDotCom/config.js
@@ -3,25 +3,27 @@ config = {
   description: "Send data to a Make webhook.",
   author: "Marko",
   blockStyle: {
-    backgroundImageUrl: 'https://youai.imgix.net/images/a8284d01-9ec8-4683-a15d-7f4d7d922e5f_1700594752696.jpg',
-    foregroundColor: '#000000',
-    label: '.',
+    backgroundImageUrl:
+      "https://youai.imgix.net/images/a8284d01-9ec8-4683-a15d-7f4d7d922e5f_1700594752696.jpg",
+    foregroundColor: "#000000",
+    label: ".",
   },
   configurationSections: [
     {
-      title: 'Configuration',
+      title: "Configuration",
       items: [
         {
-          label: 'Make Webhook URL',
-          variable: 'make_webhook_url',
-          helpText: 'Webhook URL provided by Make.',
-          type: 'text',
+          label: "Make Webhook URL",
+          variable: "make_webhook_url",
+          helpText: "Webhook URL provided by Make.",
+          type: "text",
         },
         {
-          label: 'Input',
-          variable: 'input',
-          type: 'text',
-          helpText: 'Input can be a string or a {{variable}}.',
+          label: "Input",
+          variable: "input",
+          type: "text",
+          helpText:
+            "Text sent to the webhook. Can be a string or a {{variable}}.",
         },
       ],
     },
