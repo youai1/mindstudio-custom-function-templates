8807802448d4d11e633ed79c408b451146c07d14
Victor Demin <demmbox@gmail.com>
2023-11-27 16:52:09 +0400

    wip. dreamai

---
+++ templates/generate-image-dreamai/config.js
@@ -0,0 +1,41 @@
+config = {
+  name: "Generate Image (DreamAi)",
+  description: "Use DreamAi to generate an image and save the URL.",
+  author: "Victor",
+  blockStyle: {
+    backgroundImageUrl: 'https://youai-cdn.s3.us-west-2.amazonaws.com/images/9e603bae-0732-4f04-8136-2eeec1f0a9fe_1701084184194.png',
+    foregroundColor: '#111111',
+    label: ' ',
+  },
+  configurationSections: [
+    {
+      title: 'Configuration',
+      items: [
+        {
+          label: 'DreamAI API Key',
+          variable: 'dreamai_api_key',
+          helpText: 'Your DreamAI API key.',
+          type: 'text',
+        },
+        {
+          label: 'Image Style',
+          variable: 'dreamai_style',
+          helpText: 'Number from 1 (Synthwave) to 21 (Surreal)',
+          type: 'text',
+        },
+        {
+          label: 'Prompt',
+          variable: 'dreamai_prompt',
+          type: 'text',
+          helpText: 'Prompt to use for generating the image. Can be a string or a {{variable}}.',
+        },
+        {
+          label: 'Output Variable',
+          type: 'text',
+          variable: 'outputVar',
+          helpText: 'Save the generated image URL to this variable',
+        },
+      ],
+    },
+  ],
+}
