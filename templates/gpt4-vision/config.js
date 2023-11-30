ddfbd03d5847cc2b5f550d4cd93007ee4f87034f
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2023-11-30 18:49:48 +0100

    Create config.js

---
+++ templates/gpt4-vision/config.js
@@ -0,0 +1,33 @@
+config = {
+  configurationSections: [
+    {
+      title: 'OpenAI Image Analysis',
+      items: [
+        {
+          label: 'OpenAI API Key',
+          type: 'text',
+          variable: 'openai_api_key',
+          helpText: 'Enter your OpenAI API key here',
+        },
+        {
+          label: 'Image URL',
+          type: 'text',
+          variable: 'image_url',
+          helpText: 'URL of the image to analyze',
+        },
+        {
+          label: 'Response Data Variable',
+          type: 'text',
+          variable: 'responseData',
+          helpText: 'Variable to store the response data from OpenAI',
+        },
+        {
+          label: 'Success Variable',
+          type: 'text',
+          variable: 'success',
+          helpText: 'Variable to indicate if the operation was successful',
+        },
+      ],
+    },
+  ],
+}
