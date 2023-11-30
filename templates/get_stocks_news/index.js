0a82434bdd53a4f324e37d5b87f2ba9861a29621
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2023-11-30 18:43:09 +0100

    Create index.js

---
+++ templates/get_stocks_news/index.js
@@ -0,0 +1,17 @@
+const performanceID = ai.getConfig('performanceID');
+const url = `https://morning-star.p.rapidapi.com/news/list?performanceId=${performanceID}`; // Template literal used here
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'API_KEY',
+		'X-RapidAPI-Host': 'morning-star.p.rapidapi.com'
+	}
+};
+
+
+const response = await fetch(url, options);
+const result = await response.text();
+ai.vars[ai.config.outputVar] = result
+
+
