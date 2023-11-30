534d47d7e0031985601d22ab024e4068da9ce50c
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2023-11-30 23:08:44 +0100

    Update index.js

--- templates/Detailed_stocks_news/index.js
+++ templates/Detailed_stocks_news/index.js
@@ -6,23 +6,29 @@ const url = `https://morning-star.p.rapidapi.com/news/get-details?sourceId=${sou
 const options = {
     method: 'GET',
     headers: {
-        'X-RapidAPI-Key': 'API_KEY',
+        'X-RapidAPI-Key': 'd4bf753e5amsh66b7b07033cb646p1931d2jsn5f22292991c2',
         'X-RapidAPI-Host': 'morning-star.p.rapidapi.com'
     }
 };
 
+try {
+    const response = await fetch(url, options);
+    const result = await response.json(); // Parse the response as JSON
 
-const response = await fetch(url, options);
-const result = await response.json(); // Parse the response as JSON
-
-let concatenatedContent = '';
-result.body.forEach(item => {
-    if (item.type === 'p') {
-        item.contentObject.forEach(contentItem => {
-            if (contentItem.type === 'text') {
-                concatenatedContent += contentItem.content + ' ';
+    let concatenatedContent = '';
+    for (let i = 0; i < result.body.length; i++) {
+        const item = result.body[i];
+        if (item.type === 'p') {
+            for (let j = 0; j < item.contentObject.length; j++) {
+                const contentItem = item.contentObject[j];
+                if (contentItem.type === 'text') {
+                    concatenatedContent += contentItem.content + ' ';
+                }
             }
-        });
+        }
     }
-});
-ai.vars[ai.config.outputVar] = concatenatedContent.trim()
+
+    ai.vars[ai.config.outputVar] = concatenatedContent.trim();
+} catch (error) {
+    console.error(`Error fetching data: ${error}`);
+}
