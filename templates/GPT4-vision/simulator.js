e83195c807942fbe61fae1897b7b171e62c4ab19
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2023-11-30 15:54:27 +0100

    Create simulator.js

---
+++ templates/GPT4-vision/simulator.js
@@ -0,0 +1,7 @@
+environment = {
+  config: {
+    image_url: "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg",
+    openai_api_key: 'OPENAI_API_KEY',
+    responseData: 'responseData',
+  },
+}
