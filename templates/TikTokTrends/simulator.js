73ea453f63d18af53600f94f5129e9bcf3f5b698
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2023-11-30 15:55:51 +0100

    Delete templates/TikTokTrends directory

--- templates/TikTokTrends/simulator.js
+++
@@ -1,8 +0,0 @@
-environment = {
-  config: {
-    // Variables configured by block settings
-    responseData: 'Trends',
-    region:"US",
-    rapidapi:"d4bf753e5amsh66b7b07033cb646p1931d2jsn5f22292991c2"
-  },
-}
