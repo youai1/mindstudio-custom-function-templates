e1d69992826171598451e1381f060a1912b3af40
Victor Demin <demmbox@gmail.com>
2023-12-04 20:18:28 +0400

    Calendly: improvements

--- templates/fetch-calendly-availability/config.js
+++ templates/fetch-calendly-availability/config.js
@@ -5,7 +5,7 @@ config = {
   blockStyle: {
     backgroundImageUrl: 'https://youai.imgix.net/images/9e603bae-0732-4f04-8136-2eeec1f0a9fe_1701705960604.png',
     foregroundColor: '#000000',
-    label: '',
+    label: ' ',
   },
   configurationSections: [
     {
@@ -15,7 +15,7 @@ config = {
           label: 'Calendly Personal Token',
           variable: 'calendly_token',
           helpText: 'See https://developer.calendly.com/how-to-authenticate-with-personal-access-tokens',
-          type: 'text',
+          type: 'secret',
         },
         {
           label: 'Availability Output Variable',
