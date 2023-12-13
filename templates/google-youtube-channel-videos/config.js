8378468671e166e14b91992d62e3a8af308b6b53
Marko <markofajler@gmail.com>
2023-12-13 21:13:18 +0100

    add youtube videos block

---
+++ templates/google-youtube-channel-videos/config.js
@@ -0,0 +1,35 @@
+config = {
+  name: 'Get YouTube Videos',
+  description: 'Get videos from a YouTube Channel',
+  thumbnailUrl: '',
+  blockStyle: {
+    backgroundColor: '#007aff',
+    foregroundColor: '#ffffff',
+    label: 'Get YouTube Videos',
+  },
+  configurationSections: [
+    {
+      title: 'Configuration',
+      items: [
+        {
+          label: 'Google Cloud API Key',
+          variable: 'apiKey',
+          helpText: '',
+          type: 'secret',
+        },
+        {
+          label: 'Channel ID',
+          variable: 'channelId',
+          helpText: 'Your YouTube Channel ID',
+          type: 'text',
+        },
+        {
+          label: 'Output Variable',
+          variable: 'outputVar',
+          helpText: 'Variable to assign the output to.',
+          type: 'text',
+        },
+      ],
+    },
+  ],
+}
