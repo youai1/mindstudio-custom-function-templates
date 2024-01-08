775f4ded1ef97398f3ef8cfc7863b302382b9726
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2024-01-08 10:30:29 +0100

    Update config.js

--- templates/get_stocks_news/config.js
+++ templates/get_stocks_news/config.js
@@ -2,6 +2,11 @@ config = {
   name:"Get Stocks News",
   description:"Get daily news about the desired stock",
   author:"Kevin L",
+  blockStyle: {
+    backgroundImageUrl: 'https://youai-cdn.s3.us-west-2.amazonaws.com/images/d83bf0b7-ffc0-442f-a7a2-efb97a670708_1704706180757.png',
+    foregroundColor: '#ffffff',
+    label: ' ',
+  },
   configurationSections: [
     {
       title: 'All news',
