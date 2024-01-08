e2786a7a3992fb52a2755ebc9cd946d33ad5c804
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2024-01-08 10:21:49 +0100

    Create config.js

---
+++ templates/postgresql/config.js
@@ -0,0 +1,36 @@
+config = {
+  name:"PostGreSQL",
+  description:"Talk to your postgresql database",
+  author:"Kevin L",
+  thumbnailUrl: 'https://youai-cdn.s3.us-west-2.amazonaws.com/images/d83bf0b7-ffc0-442f-a7a2-efb97a670708_1704705298924.png',
+  blockStyle: {
+    backgroundImageUrl: 'https://youai-cdn.s3.us-west-2.amazonaws.com/images/d83bf0b7-ffc0-442f-a7a2-efb97a670708_1704705298924.png',
+    foregroundColor: '#000000',
+    label: ' ',
+  },
+  configurationSections: [
+    {
+      title: 'Configuration',
+      items: [
+        {
+          label: 'Database URL',
+          variable: 'database_url',
+          helpText: 'The URL of your PostgreSQL database.',
+          type: 'text',
+        },
+        {
+          label: 'SQL Query',
+          variable: 'query',
+          helpText: 'The SQL query to execute.',
+          type: 'text',
+        },
+        {
+          label: 'Response Data Variable',
+          type: 'text',
+          variable: 'queryResult',
+          helpText: 'Store query result, name this as queryResult',
+        },
+      ],
+    },
+  ],
+}
