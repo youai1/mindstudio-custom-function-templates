2fbda21c10708d8d6f3071d1279ea99e4ab427f2
Kevin Lopez <104493450+Braingearceo@users.noreply.github.com>
2024-01-23 08:08:59 +0100

    Update config.js

--- templates/gpt4-vision/config.js
+++ templates/gpt4-vision/config.js
@@ -1,6 +1,6 @@
 config = {
   name:"GPT4-Vision",
-  description:"Provide any image and GPT4V will describe it.",
+  description:"Provide any image and GPT4V will describe it based on your instructions",
   author:"Kevin L",
   blockStyle: {
     backgroundImageUrl: 'https://youai-cdn.s3.us-west-2.amazonaws.com/images/d83bf0b7-ffc0-442f-a7a2-efb97a670708_1702632143584.png',
@@ -22,6 +22,12 @@ config = {
           type: 'text',
           variable: 'image_url',
           helpText: 'URL of the image to analyze',
+        },
+        {
+          label: 'GPT-4 Vision Instructions',
+          type: 'text',
+          variable: 'instructions',
+          helpText: 'Text used to prompt GPT-4 Vision.\n(e.g. "Summarize this image.")'
         },
         {
           label: 'Response Data Variable',
