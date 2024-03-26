04f019f9f3e3fb18277a54542cf2665c79a6f353
Marko <markofajler@gmail.com>
2024-03-26 19:50:49 +0100

    add error logging for dalle

--- templates/generate-image-dalle/index.js
+++ templates/generate-image-dalle/index.js
@@ -1,30 +1,30 @@
-const apiKey = ai.getConfig("openai_api_key");
-const model = ai.getConfig("model");
+const apiKey = ai.getConfig('openai_api_key');
+const model = ai.getConfig('model');
 
 // Read the variable provided to get the prompt
-const prompt = ai.getConfig("prompt");
+const prompt = ai.getConfig('prompt');
 
 if (!prompt) {
-  ai.crmLog("No prompt to send to Open AI.");
+  ai.crmLog('No prompt to send to Open AI.');
   return;
 }
 
 const defaultModel = 'dall-e-3';
 
 // Use a default model in case one is not provided
-const resolvedModel = model && `${model}`.trim() !== "" ? model : defaultModel;
+const resolvedModel = model && `${model}`.trim() !== '' ? model : defaultModel;
 
 const data = {
   model: resolvedModel,
   prompt,
   n: 1,
-  size: "1024x1024",
-  response_format: 'b64_json'
-}
+  size: '1024x1024',
+  response_format: 'b64_json',
+};
 
 const url = `https://api.openai.com/v1/images/generations`;
 
-ai.log("Generating image...");
+ai.log('Generating image...');
 
 try {
   // Make a POST request to the OpenAi API
@@ -32,20 +32,29 @@ try {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'Authorization': `Bearer ${apiKey}`
+      Authorization: `Bearer ${apiKey}`,
     },
-    body: JSON.stringify(data)
+    body: JSON.stringify(data),
   });
 
   const response = await request.json();
 
+  if (response.error) {
+    ai.vars[ai.config.outputVar] = `${response.error.message}`;
+    ai.crmLog(response.error.message);
+    return;
+  }
   // Upload the image
-  const responseUrl = await ai.uploadFile(`${response.data[0].b64_json}`, 'image/png', 'base64');
+  const responseUrl = await ai.uploadFile(
+    `${response.data[0].b64_json}`,
+    'image/png',
+    'base64',
+  );
 
   // Set the URL as a variable
   ai.vars[ai.config.outputVar] = `${responseUrl}?w=1024`;
 
   ai.crmLog(`Image generated: ${responseUrl}?w=1024`);
 } catch (err) {
-  console.error("Error during POST request.");
+  console.error('Error during POST request.');
 }
